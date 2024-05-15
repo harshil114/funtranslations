14c858668db3923691db7495545c99eefdd44ca3
harshil114 <harshilvispute114@gmail.com>
2024-05-15 13:43:12 +0530

    add serverURL and fetch the URL

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 let inputDiv = document.querySelector(".container-value--inputarea");
 let button = document.querySelector(".container-value--btn");
 let outputDiv = document.querySelector(".container-value--output");
+let serverURL = "https://api.funtranslations.com/translate/yoda.json";
+ 
+function getURL(input){
+    return serverURL + "?" + "text=" + input;
+}
 
 button.addEventListener("click",function clickHandler(){
-    console.log("clicked");
+   let inputText = inputDiv.value;
+   fetch(getURL(inputText))
+   .then(response => response.json())
+   .then(json=>{
+    let translatedText = json.contents.translated;
+    console.log(translatedText);
+   })
 });
