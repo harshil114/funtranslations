b5c304bfa3c01fbdf4620819a71067e6375e2b0c
harshil114 <harshilvispute114@gmail.com>
2024-05-15 13:33:02 +0530

    add button with JavaScript wiring

---
+++ app.js
@@ -0,0 +1,7 @@
+let inputDiv = document.querySelector(".container-value--inputarea");
+let button = document.querySelector(".container-value--btn");
+let outputDiv = document.querySelector(".container-value--output");
+
+button.addEventListener("click",function clickHandler(){
+    console.log("clicked");
+});
