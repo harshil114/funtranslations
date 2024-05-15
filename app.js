3e4d972e670b518379bff6961923b2df70ffb80e
harshil114 <harshilvispute114@gmail.com>
2024-05-15 14:21:05 +0530

    change button class to id

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let inputDiv = document.querySelector(".container-value--inputarea");
-let button = document.querySelector(".container-value--btn");
+let buttonYoda = document.querySelector("#container-value--btn-yoda");
 let outputDiv = document.querySelector(".container-value--output");
-let serverURL = "https://apifuntranslations.com/translate/yoda.json";
+let YodaServerURL = "https://api.funtranslations.com/translate/yoda.json";
  
 function getURL(input){
-    return serverURL + "?" + "text=" + input;
+    return YodaServerURL + "?" + "text=" + input;
 }
 function errorHandler(error){
     alert("Try again later");
 }
 
-button.addEventListener("click",function clickHandler(){
+buttonYoda.addEventListener("click",function clickHandler(){
    let inputText = inputDiv.value;
    fetch(getURL(inputText))
    .then(response => response.json())
