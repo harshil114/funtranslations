a983f076335a52465277f5c0f4bd639ed541a935
harshil114 <harshilvispute114@gmail.com>
2024-06-01 19:36:47 +0530

    add doge.js

---
+++ javascript/doge.js
@@ -0,0 +1,19 @@
+let inputDiv = document.querySelector(".container-value--inputarea");
+let buttonDoge = document.querySelector("#container-value--btn-doge");
+let outputDiv = document.querySelector(".container-value--output");
+let YodaServerURL = "https://api.funtranslations.com/translate/doge.json";
+
+
+function getURL(input){
+    return YodaServerURL + "?" + "text=" + input;
+}
+function clickHandler(){
+    let inputText = inputDiv.value;
+    fetch(getURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        let translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+}
+buttonDoge.addEventListener("click",clickHandler);
